574d5b1340ae180c9c7b84120b791b3e71c669af
Hector <thehectorr.05@gmail.com>
2024-07-18 00:17:31 -0400

    funcion para calcular el factorial

--- factorial.c
+++ factorial.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Funcion para calcular el factorial de un numero
+unsigned long long factorial(int n) {
+    if (n < 0) {
+        printf("No existe el factorial de un numero negativo.\n");
+        return 0;
+    }
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
 
